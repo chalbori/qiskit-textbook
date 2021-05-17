344ecb7b2bce017c0f578ed35d6ad4903c28a034
chalbori <sw.lee@yonsei.ac.kr>
2021-05-18 00:35:19 +0900

    Update setup.py

--- qiskit-textbook-src/setup.py
+++ qiskit-textbook-src/setup.py
@@ -9,10 +9,12 @@ setup(
   the Qiskit Textbook. See the textbook and a list of contributors at qiskit.org/textbook''',
   packages=find_packages(),
   install_requires=[
-    'qiskit',
+    'qiskit==0.23.2',
     'ipython',
     'ipywidgets',
     'numpy',
-    'matplotlib'
+    'matplotlib',
+    'torch==1.3.1',
+    'torchvision==0.4.2',
   ]
 )
